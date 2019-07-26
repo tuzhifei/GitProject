3e2ef6b126f2f8e3189e5e2364f9a0a5a6c1496a
tuzhifei@sunmi.com <tuzhifei@sunmi.com>
2019-07-26 11:34:07 +0800

    Initial commit.

---
+++ GitProject/GitProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitProject
+//
+//  Created by mark on 2019/7/26.
+//  Copyright © 2019 mark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitProject/GitProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitProject
+//
+//  Created by mark on 2019/7/26.
+//  Copyright © 2019 mark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
